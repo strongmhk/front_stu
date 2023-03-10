0b10f43be3e58a2644b63b9588d5da3ab1d4045a
Minhyung Kim <kim6562166086@gmail.com>
2023-03-10 16:36:17 +0900

    variable

---
+++ nomad/vanilajs/2_variable.js
@@ -0,0 +1,9 @@
+const a = 5; // num
+const b = "2"; // text
+const c = 2;
+const myname = "minhyung";
+
+console.log(a / c);
+console.log(a + c);
+console.log(a * c);
+console.log("hello " + myname);
