ca22d91524f05b2f71f8c2ca1de6a664814a2c86
Minhyung Kim <kim6562166086@gmail.com>
2023-03-10 22:17:38 +0900

    delete 2_variable.js

--- nomad/vanilajs/2_variable.js
+++
@@ -1,9 +0,0 @@
-const a = 5; // num
-const b = "2"; // text
-const c = 2;
-const myname = "minhyung";
-
-console.log(a / c);
-console.log(a + c);
-console.log(a * c);
-console.log("hello " + myname);
