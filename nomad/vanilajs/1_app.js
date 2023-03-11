4ca1f28a9e6589b07a38f870d6882ec4fa729266
Minhyung Kim <kim6562166086@gmail.com>
2023-03-11 17:25:22 +0900

    modified code

--- nomad/vanilajs/1_app.js
+++ nomad/vanilajs/1_app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const player = {
+  name : "minhyung",
+  age : 24,
+};
+
+console.log(player);
+
+player.name = "Kim";
+console.log(player);
+
+player.sexy = true;
+console.log(player, console);
--- nomad/vanilajs/4_function.js
+++ nomad/vanilajs/4_function.js
@@ -1,28 +1,67 @@
-function sayHello(name, age){
-  console.log("Hello my name is " + name + " and I'm  " + age);
-}
+// function sayHello(name, age){
+//   return("Hello my name is " + name + " and I'm  " + age);
+// }
 
-function plus(a, b){
-  return a + b; // return값 주기
-}
+// function plus(a, b){
+//   return a + b; // return값 주기
+// }
 
-const name1 = "minhyung";
-const age1 = 24;
+// const name1 = "minhyung";
+// const age1 = 24;
 
-const player = {
-  name : name1,
-  sayHello : function(otherName){
-    console.log("Hello! " + otherName); 
+// const player = {
+//   name : name1,
+//   sayHello : function(otherName){
+//     return("Hello! " + otherName); 
+//   }
+// }; //  객체 안에 메소드 정의
+
+// const f = plus(10, 5);
+// return(f);
+// return(player.name);
+// player.sayHello("lynn");
+
+// sayHello(name1, age1); 
+
+
+
+// 계산기 객체
+const calculator = {
+  add : function(a, b){
+    return a + b;
+  },
+  minus : function(a, b){
+    return a - b;
+  },
+  divide : function(a, b){
+    return a / b;
+  },
+  mul : function(a, b){
+    return a * b;
+  },
+  powerof : function(a, b){
+    return a ** b;
   }
-}; //  객체 안에 메소드 정의
+};
 
 
 
-const f = plus(10, 5);
-console.log(f);
-console.log(player.name);
-player.sayHello("lynn");
 
-alert();
-sayHello(name1, age1); 
+const age = 96;
+function CalcKrAge(fage){
+  return fage + 2;
+}
+
+krAge = CalcKrAge(age); // return 값 이용
+console.log(krAge);
+
+const plusResult = calculator.plus(2, 4);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.mul(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+console.log(result);
+
+
 
