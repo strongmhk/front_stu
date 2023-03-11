77513ead966d03f0bd01ba0e460e4aeaf12c638b
Minhyung Kim <kim6562166086@gmail.com>
2023-03-11 16:39:07 +0900

    add recap and conditional

---
+++ nomad/vanilajs/5_conditional.js
@@ -0,0 +1,9 @@
+const age = parseInt(prompt("How old are you ? : ")); // c의 scanf랑 비슷한 역할을 하는 함수, string값을 받아옴, 요즘은 사용 잘 안함. 
+
+// parseInt : data type을 string -> num으로변환
+
+console.log(age);
+
+// console.log(typeof parseInt("15"));
+// console.log(typeof age, typeof parseInt(age));
+
