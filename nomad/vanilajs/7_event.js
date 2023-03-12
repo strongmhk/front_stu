bbf142fd93ce7ac09f26288a1e15575685dc2544
Minhyung Kim <kim6562166086@gmail.com>
2023-03-13 00:25:30 +0900

    input event

--- nomad/vanilajs/7_event.js
+++ nomad/vanilajs/7_event.js
@@ -1,8 +1,18 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-  title.style.color = "blue";
+  title.style.color = "green";
   // console.log("title was clicked!");
 }
 
 title.addEventListener("click", handleTitleClick);
+
+// function clickbox(){
+//   box.style.borderBlockColor = "green";
+// }
+
+// const box = document.querySelector(".box");
+// console.dir(box);
+
+// box.addEventListener("input", clickbox);
+
