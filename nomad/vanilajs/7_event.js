e54677fc3ae5fc8aeac809c3f49819d44486b203
Minhyung Kim <kim6562166086@gmail.com>
2023-03-12 23:59:43 +0900

    event!

---
+++ nomad/vanilajs/7_event.js
@@ -0,0 +1,8 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+  title.style.color = "blue";
+  // console.log("title was clicked!");
+}
+
+title.addEventListenerr("click", handleTitleClick);
